50624e36f9857dd51959e00b69720eb4fca986ad
zawakou <zawakou@zawk.net>
2023-07-08 02:46:55 +0900

    get-token

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,37 @@
 use dotenv;
 use reqwest::header::{HeaderMap, HeaderValue, COOKIE};
+use serde_json::Value;
 
 #[tokio::main]
 async fn main() -> Result<(), reqwest::Error> {
-    let mut url = dotenv::var("url").unwrap();
-    url.push_str("json/health_temperature");
-    let cookie = "sessionKey=";
-    let mut headers = HeaderMap::new();
-    headers.insert(COOKIE, HeaderValue::from_str(cookie).unwrap());
+    let login_res: Result<String, Box<dyn std::error::Error>> = login().await;
+    match login_res {
+        Ok(s) => {
+            let u: &str = s.as_str();
+            let v: Value = serde_json::from_str(u).unwrap();
+            println!("{}", v["session_key"]);
+        }
+        Err(e) => {
+            println!("{}", e);
+        }
+    }
+
+    Ok(())
+}
+
+async fn login() -> Result<String, Box<dyn std::error::Error>> {
+    let url = dotenv::var("url").unwrap() + "json/login_session";
+    let user = dotenv::var("user").unwrap();
+    let pw = dotenv::var("pw").unwrap();
+    let body = format!(
+        "{{\"method\":\"login\",\"user_login\":\"{}\",\"password\":\"{}\"}}",
+        user, pw
+    );
 
     let client = reqwest::Client::builder()
         .danger_accept_invalid_certs(true)
-        .default_headers(headers)
         .build()?;
+    let res = client.post(&url).body(body).send().await;
 
-    let res = client.get(url).send().await?.text().await?;
-
-    println!("{}", res);
-    Ok(())
+    Ok(res?.text().await?)
 }
