9764d190b84b1b74e7dacecc4c44d1b70fdf4f75
zawakou <zawakou@zawk.net>
2023-07-08 13:43:27 +0900

    get-temp

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,19 @@ async fn main() -> Result<(), reqwest::Error> {
         Ok(s) => {
             let u: &str = s.as_str();
             let v: Value = serde_json::from_str(u).unwrap();
-            println!("{}", v["session_key"]);
+            let temp_res: Result<String, Box<dyn std::error::Error>> =
+                get_temp(&v["session_key"].to_string()).await;
+
+            match temp_res {
+                Ok(s) => {
+                    let u: &str = s.as_str();
+                    let v: Value = serde_json::from_str(u).unwrap();
+                    println!("{}", v)
+                }
+                Err(e) => {
+                    println!("{}", e);
+                }
+            }
         }
         Err(e) => {
             println!("{}", e);
@@ -17,6 +29,20 @@ async fn main() -> Result<(), reqwest::Error> {
     }
 
     Ok(())
+}
+
+async fn get_temp(token: &String) -> Result<String, Box<dyn std::error::Error>> {
+    let url = dotenv::var("url").unwrap() + "json/health_temperature";
+    let cookie = format!("sessionKey={}", token.replace("\"", ""));
+    let mut headers = HeaderMap::new();
+    headers.insert(COOKIE, HeaderValue::from_str(&cookie).unwrap());
+    let client = reqwest::Client::builder()
+        .danger_accept_invalid_certs(true)
+        .default_headers(headers)
+        .build()?;
+    let res = client.get(&url).send().await;
+
+    Ok(res?.text().await?)
 }
 
 async fn login() -> Result<String, Box<dyn std::error::Error>> {
